78300e011b12d8b403f87f16f7b99c95e7c747df
Andro Tech <khbu2017@gmail.com>
2022-11-26 16:59:42 +0200

    11

---
+++ Final-Project/database/migrations/2022_11_26_144502_managers.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('managers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('username');
+            $table->text('email');
+            $table->text('password');
+            $table->text('phone_number');
+            $table->text('visa_card');
+            $table->text('user_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('managers');
+    }
+};
