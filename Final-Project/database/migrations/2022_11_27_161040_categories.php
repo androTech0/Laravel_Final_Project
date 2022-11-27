24db73fd76c9002ce14cb6f0718c0712e7613b93
Andro Tech <khbu2017@gmail.com>
2022-11-27 18:24:47 +0200

    13

---
+++ Final-Project/database/migrations/2022_11_27_161040_categories.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('category_name');
+            $table->text('category_logo');
+            $table->text('products_count');
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+};
